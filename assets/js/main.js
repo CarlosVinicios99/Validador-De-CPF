0544f183c26705f46de75cfef683715a4c045afa
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-03-08 14:14:30 -0300

    funcao para separar digitos do cpf e adicionalos em ordem num array

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function separaDigitos(cpf){
+    const digitosCpf = []
+    for(let i = 0; i < cpf.length; i++){
+        digitosCpf.push(cpf.charAt(i))
+    }
+    return digitosCpf
+}
+
+
+const botaoValidar = 
