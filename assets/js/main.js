c5dddcf98753f0259598dd1a51c41b19238983e9
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-03-08 14:49:30 -0300

    funcoes gerais do algoritmo declaradas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,53 @@
-function separaDigitos(cpf){
-    const digitosCpf = []
-    for(let i = 0; i < cpf.length; i++){
-        digitosCpf.push(cpf.charAt(i))
-    }
-    return digitosCpf
+function obterCpfDigitado(){
+    const cpf = document.querySelector("#cpf")
+    return cpf.value
 }
 
+function separarDigitos(cpf){
+    const digitosCpf = []
+    for(let i = 0; i < cpf.length - 2; i++){
+        digitosCpf.push(cpf.charAt(i))
+    }
+    return digitosDoCpf
+}
 
-const botaoValidar = 
+function calcularPrimeiroDigito(digitosDoCpf){
+    //return primeiroDigito
+}
+
+function calcularSegundoDigito(digitosDoCpf){
+    //return segundoDigito
+}
+
+function agruparDigitos(digitosDoCpf){
+    //return cpf calculado
+}
+
+function verificarCpf(cpfDigitado, cpfCalculado){
+    //return verificacao
+}
+
+function exibirResultado(verificacao){
+    //mostrar mensagem da verficiacao na div de resultado
+}
+
+(function(){
+    const botaoValidar = document.querySelector("#validar")
+
+    botaoValidar.addEventListener('click', function(e){
+        e.preventDefault()
+        const cpfDigitado = obterCpfDigitado()
+        const digitosDoCpf = separarDigitos(cpfDigitado)
+
+        const primeiroDigito = calcularPrimeiroDigito(digitosDoCpf)
+        digitosDoCpf.push(primeiroDigito)
+
+        const segundoDigito = calcularSegundoDigito(digitosDoCpf)
+        digitosDoCpf(segundoDigito)
+    
+        const cpfCalculado = agruparDigitos(digitosDoCpf)
+        const verificacao = verificarCpf(cpfDigitado, cpfCalculado)
+        exibirResultado(verificacao)
+    })
+})()
+
